c5d604cfebd385c79818f0f5cb69340d267fecb2
ausilianapoli <ausilianapoli@gmail.com>
2020-09-27 19:54:54 +0200

    initial style

--- resources/views/notes/show.blade.php
+++ resources/views/notes/show.blade.php
@@ -1,6 +1,26 @@
 @extends('layout')
 
-@section('serial')
+@section('banner')
+
+    <section class="hero-banner">
+        <div class="container">
+            <h2>Your Note is 
+            @if($note[0]->valid == true)
+                <span class="basecolor">valid</span>
+            @else
+                <span class="basecolor">not valid</span>
+            @endif
+            </h2>
+        </div>
+    </section>
+
+    <section class="area-padding">
+        <div class="container">
+            <div class="row">
+                
+            </div>
+        </div>
+    </section>
 
 <h1 class="title">
     {{ $note[0]->serial }} is 
@@ -16,4 +36,4 @@
 <p>{{ $n->value }}€ - {{ $n->zip }} - {{ $n->created_at }}</p>
 @endforeach
 
-@endsection('serial')
+@endsection('banner')
