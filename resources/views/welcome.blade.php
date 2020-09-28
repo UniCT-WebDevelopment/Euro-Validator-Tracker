773e003e14ee838d616d3d2dd527c461b5fb7d97
ausilianapoli <ausilianapoli@gmail.com>
2020-09-28 17:20:22 +0200

    update footer

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -24,4 +24,20 @@
             <div></div></div>
     </section>
 
+    <footer class="footer-area section_gap">
+        <div class="container">
+            <div class="row">
+                <div class="col-12">
+                    <p class="footer-text m-0"><!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. -->
+                    Copyright &copy;<script>document.write(new Date().getFullYear());</script> All rights reserved | This template is made with <i class="fa fa-heart" aria-hidden="true"></i> by <a href="https://colorlib.com" target="_blank">Colorlib</a>
+                    <!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. --></p>
+                    <p class="footer-text">Be social with 
+                        <a href="https://www.linkedin.com/in/maria-ausilia-napoli-spatafora-b529b036/"><i class="ti-linkedin"></i></a> 
+                        <a class="twitter" href="https://github.com/ausilianapoli"><i class="ti-github"></i></a>
+                    </p>
+                </div>
+            </div>
+        </div>    
+    </footer>
+
 @endsection('banner')
